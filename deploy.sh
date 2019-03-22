02c8e80ea6cef5047c78716eb11ff4efb9a6402b
markjones <mark.jones@mab.org.uk>
2019-03-22 17:47:02 +0000

    Inital commit of scripts

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.aws
+mkdir -p ~/.docker
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
+
+cat > ~/.aws/config << EOL
+[default]
+output = json
+region = ${AWS_REGION}
+EOL
+
+echo $RSA_KEY > ~/.docker/rsakey.pem
+chmod 400 ~/.docker/rsakey.pem
+
+aws cloudformation create-stack --stack-name rookstack --template-url https://editions-us-east-1.s3.amazonaws.com/aws/stable/Docker.tmpl --region eu-west-1 --parameters ParameterKey=KeyName,ParameterValue=rsakey ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=ManagerInstanceType,ParameterValue=t2.micro ParameterKey=ClusterSize,ParameterValue=3 --capabilities CAPABILITY_IAM^C 
+
+MANAGER_INSTANCE=`aws ec2 describe-instances --query 'Reservations[0].Instances[0].{ID:InstanceId}' | grep ID | awk -F ":" '{print $2}' | sed 's/[",]//g'`
+IP=`aws ec2 describe-instances --instance-ids $MANAGER_INSTANCE | grep PublicIpAddress | awk -F ":" '{print $2}' | sed 's/[",]//g'`
+
+ssh - .~/.docker/rsakey.pem -NL localhost:2374:/var/run/docker.sock docker@$IP
+set DOCKER_HOST=localhost:2374
+
+docker stack deploy 
