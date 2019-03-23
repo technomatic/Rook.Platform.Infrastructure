763aeca4fe97b4d9329aa4928b0ffe6a4a690ca8
markjones <mark.jones@mab.org.uk>
2019-03-23 20:00:42 +0000

    trying to fix the delay in the build

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,43 @@ region = ${AWS_REGION}
 EOL
 
 aws cloudformation create-stack --stack-name rookstack --template-url https://editions-us-east-1.s3.amazonaws.com/aws/stable/Docker.tmpl --region eu-west-1 --parameters ParameterKey=KeyName,ParameterValue=rsakey ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=ManagerInstanceType,ParameterValue=t2.micro ParameterKey=ClusterSize,ParameterValue=3 --capabilities CAPABILITY_IAM 
-aws cloudformation wait stack-create-complete --stack-name rookstack
+#aws cloudformation wait stack-create-complete --stack-name rookstack
+
+
+stackStatus="CREATE_IN_PROGRESS"
+
+while [[ 1 ]]; do
+    echo aws cloudformation describe-stacks --region eu-west-1 --stack-name rookstack
+    response=$(aws cloudformation describe-stacks --region eu-west-1 --stack-name rookstack 2>&1)
+    responseOrig="$response"
+    response=$(echo "$response" | tr '\n' ' ' | tr -s " " | sed -e 's/^ *//' -e 's/ *$//')
+
+    if [[ "$response" != *"StackStatus"* ]]
+    then
+        echo "Error occurred creating AWS CloudFormation stack. Error:"
+        echo "    $responseOrig"
+        exit -1
+    fi
+
+    stackStatus=$(echo $response | sed -e 's/^.*"StackStatus"[ ]*:[ ]*"//' -e 's/".*//')
+    echo "    StackStatus: $stackStatus"
+
+    if [[ "$stackStatus" == "ROLLBACK_IN_PROGRESS" ]] || [[ "$stackStatus" == "ROLLBACK_COMPLETE" ]] || [[ "$stackStatus" == "DELETE_IN_PROGRESS" ]] || [[ "$stackStatus" == "DELETE_COMPLETE" ]]; then
+        echo "Error occurred creating AWS CloudFormation stack and returned status code ROLLBACK_IN_PROGRESS. Details:"
+        echo "$responseOrig"
+        exit -1
+    elif [[ "$stackStatus" == "CREATE_COMPLETE" ]]; then
+        break
+    fi
+
+    # Sleep for 5 seconds, if stack creation in progress
+    sleep 5
+done
+
+
+
+
+
 aws ec2 describe-instances --query 'Reservations[0].Instances[0].{ID:InstanceId}' | grep ID | awk -F ":" '{print $2}' | sed 's/[",]//g'
 MANAGER_INSTANCE=`aws ec2 describe-instances --query 'Reservations[0].Instances[0].{ID:InstanceId}' | grep ID | awk -F ":" '{print $2}' | sed 's/[",]//g'`
 echo $MANAGER_INSTANCE
